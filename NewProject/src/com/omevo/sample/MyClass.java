304992bb4755fc1337248508c245758b4ca6461a
asa <asa@CIT-DT-025.cloveit.local>
2017-08-23 13:15:11 +0530

    hi test commit

---
+++ NewProject/src/com/omevo/sample/MyClass.java
@@ -0,0 +1,10 @@
+package com.omevo.sample;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to eclipse:");
+		System.out.println("Welcome to eclipse:");
+	}
+}
